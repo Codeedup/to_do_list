1b7285a2f2aeccbd52b375d55bc7306639d4e5c8
Dylan Moffett <dylanmoffett@hotmail.com>
2025-02-25 21:55:52 +0900

    Adding all the other features so far.

---
+++ utilities.py
@@ -0,0 +1,9 @@
+# ideally i would add helper functions here but im not 100% certain i will do it for this first project
+
+# formats tasks dict into clean string 
+
+# checks if input is valid 
+
+# file exists 
+
+# user confirmation for removal 
